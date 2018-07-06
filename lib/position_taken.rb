b44dbce4140ceeee22919e559db7c89791cea2ec
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-06 11:40:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if(index == nil)
+  if(index == nil || board[index] == nil)
     return FALSE
   end
   if(board[index].upcase == "X" || board[index].upcase == "O")
